0652bda6130f838a5cae5985d245bf3ca444299f
y0105877 <l.claussen@tu-braunschweig.de>
2023-05-31 15:45:43 +0200

    can bind exe's to button with dict -> need xml

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,10 +27,14 @@ namespace GuiTest
         public new string TextInput { get; set; }
 
         public List<string> DefaultSearchDirectories = new List<string>() { "C:\\Program Files (x86)", "C:\\Program Files", "C:\\Windows" };
+
+        public Dictionary<string, List<string>> myDictionary = new();
+        
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
+            myDictionary.Add("button1_2", new List<string>());
             //Button1.Content = "Hallo";
         }
         
@@ -44,14 +48,31 @@ namespace GuiTest
             }
             catch (Exception ex)
             {
+                Trace.WriteLine(ex.ToString());
                 return;
             }
-            _ = Process.Start(path);
+
+            try
+            {
+                _ = Process.Start(path);
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine("Acces denied: " + ex.ToString());
+            }
+
+            myDictionary["button1_2"].Add(path);
         }
 
         private void button1_2_Click(object sender, RoutedEventArgs e)
         {
-            _ = Process.Start("C:\\Program Files\\texstudio\\texstudio.exe");
+            string _identifier = "button1_2";
+
+            foreach (string path in myDictionary[_identifier])
+            {
+                _ = Process.Start(path);
+            }
+           // _ = Process.Start("C:\\Program Files\\texstudio\\texstudio.exe");
         }
 
         private string GetAppPath(string keyword, List<string> directories)
@@ -67,7 +88,7 @@ namespace GuiTest
                 }
                 catch(Exception exc)
                 {
-                    Console.WriteLine("Get Files" + exc.ToString());
+                    Trace.WriteLine("Get Files" + exc.ToString());
                 }
             }
 
@@ -88,11 +109,18 @@ namespace GuiTest
                     }
                     catch(Exception ex)
                     {
-                        Console.WriteLine("Get Directories: " + ex.ToString());
+                        Trace.WriteLine("Get Directories: " + ex.ToString());
                     }
                 }
                 //throw new Exception("No executable found."); 
-                return GetAppPath(keyword, NewDirectories);
+                if (NewDirectories.Any())
+                {
+                    return GetAppPath(keyword, NewDirectories);
+                }
+                else
+                {
+                    throw new Exception("No application found!");
+                }
 
             }
         }
